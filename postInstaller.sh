ead2c507cbb7e324ff922acf4fbf1f5d7afe97ec
Bisanota <74116034+Bisanota@users.noreply.github.com>
2025-09-27 20:07:54 -0500

    Update postInstaller.sh

--- postInstaller.sh
+++ postInstaller.sh
@@ -58,7 +58,8 @@ fi
 # Chaotic AUR y multilib
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
-pacman -S --noconfirm chaotic-keyring chaotic-mirrorlist
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 echo -e "\n#Multilib\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 echo -e "\n#Chaotic AUR\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
