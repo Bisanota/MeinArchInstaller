839cbe09fe7a551c7712ba621fc6b2e7b2145d20
Bisanota <74116034+Bisanota@users.noreply.github.com>
2026-01-05 18:53:07 -0500

    Update master.sh
    y from -Sy removed in $dependencies

--- GUI_Test/master.sh
+++ GUI_Test/master.sh
@@ -15,7 +15,7 @@ echo "Warning, you've started the installing process"
 echo "If you have run this script before, then, press Ctrl-C and just run first.sh script, otherwise, just press Enter button"
 sleep 3
 pacman -Sy archlinux-keyring --noconfirm
-pacman -Sy $dependencies --noconfirm
+pacman -S $dependencies --noconfirm
 git clone https://github.com/Bisanota/MeinArchInstaller.git
 cd MeinArchInstaller/GUI_Test
 bash first.sh
