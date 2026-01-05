74add88cdfb0e9db29aa2e90294b12be4fc08955
Bisanota <74116034+Bisanota@users.noreply.github.com>
2026-01-05 15:00:39 -0500

    Update master.sh
    Added first part  and guess is finished this script. If something is needed, then I guess is just the dependencies part to add packages just for installation.

--- GUI_Test/master.sh
+++ GUI_Test/master.sh
@@ -5,10 +5,18 @@
 #  So, I almost forgot this, IS INTENDED JUST TO RUN
 #  ON LIVE ENVIRONMENT, IS JUST THE FIRST INSTALLATION.
 # Variables
-dependencies="archlinux-keyring dialog"
+dependencies="dialog git"
 # Functions
 
 
 # Code
+clear
+echo "Warning, you've started the installing process"
+echo "If you have run this script before, then, press Ctrl-C and just run first.sh script, otherwise, just press Enter button"
+echo "Press Enter to continue"
+read
+pacman -Sy archlinux-keyring
 pacman -Sy $dependencies
+
+bash first.sh
 
