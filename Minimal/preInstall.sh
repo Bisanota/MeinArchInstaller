f67a3833d007b829e2450512a02dafca58b757db
Bisanota <74116034+Bisanota@users.noreply.github.com>
2026-01-07 22:27:11 -0500

    Create preInstall.sh
    Just a minimal installation

---
+++ Minimal/preInstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Debloated Installer with packages I'll use personally
+
+core=" nano networkmanager htop fastfetch grub os-prober efibootmgr zram-generator git less bash-completion"
+
+echo "Is suppossed partitions are mounted, right?"
+echo "Press ctrl-c if you want not to continue, another key for continue"
+read
+
+installArch="pacstrap -K /mnt $paquetes"
+tries=0
+maxTries=5
+hasWorked=0
+
+while [ $tries -le $maxTries ] ; do
+
+    $installArch && break
+    echo "Has failed :c"
+    tries=$((intentos + 1))
+    hasWorked=1
+done
+
+if [ $hasWorked == 0 ]; then
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Next part relies on you"
+echo "Good luck"
+echo "Copy installerPartII from parent folder"
+
+fi
